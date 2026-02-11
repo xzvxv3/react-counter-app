6e2290a254ab1d3f1cd7356ee62951adfa1a60a0
lgh02 <xzvxv3@gmail.com>
2026-02-11 15:19:29 +0900

    UI 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import "./App.css";
 import Viewer from "./components/Viewer";
+import Controller from "./components/Controller";
 
 function App() {
   return (
-    <>
-      <Viewer />
-    </>
+    <div className="App">
+      <h1>Simple Counter</h1>
+
+      <section>
+        <Viewer />
+      </section>
+
+      <section>
+        <Controller />
+      </section>
+    </div>
   );
 }
 
--- src/components/Viewer.jsx
+++ src/components/Viewer.jsx
@@ -1,5 +1,10 @@
 const Viewer = () => {
-  return <div>viewer</div>;
+  return (
+    <div>
+      <div>현재 카운트: </div>
+      <h1>0</h1>
+    </div>
+  );
 };
 
 export default Viewer;
